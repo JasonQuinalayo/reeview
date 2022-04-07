f7b48d249d91f891bb3652b9fe76da2bbdcc639c
Jason Quinalayo <quinalayojason@gmail.com>
2022-04-07 00:26:42 -0400

    dbugging

--- back-end/app.js
+++ back-end/app.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.static('build'));
 app.use('/api/register', registerRouter);
 app.use('/api/auth', authRouter);
-app.use((req, res, next) => { console.log(req.user); next(); });
+app.use((req, res, next) => { console.log(req.sessionID); next(); });
 app.use(middleware.authWall);
 app.use('/api/user', userRouter);
 app.use('/api/questions', questionsRouter);
